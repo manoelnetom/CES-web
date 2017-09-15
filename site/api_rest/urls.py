26f47da51ebb224418a543b5a428539e67be0bf6
Brunno Barbosa Mendes <brunnomendesinfo@gmail.com>
2017-09-15 19:01:10 -0300

    - Corrigir o nome da url movimentacao_aberta

--- site/api_rest/urls.py
+++ site/api_rest/urls.py
@@ -4,7 +4,7 @@ from api_rest import views
 
 app_name = 'api_rest'
 urlpatterns = [
-    url(r'^movimentacoes_aberto/(?P<user_id>\d+)', views.MovimentacaoAbertaServiceView.as_view(), name='movimentacoes_aberto'),
+    url(r'^movimentacao_aberta/(?P<user_id>\d+)', views.MovimentacaoAbertaServiceView.as_view(), name='movimentacoes_aberto'),
     url(r'^devolver_objeto/', views.DevolverObjetoServiceView.as_view(), name='devolver_objeto'),
     url(r'^exibir_objetos/', views.ObjetoServiceView.as_view(), name='exibir_objetos'),
 ]
