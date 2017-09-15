07f63ad63f111cddfe9565cb836350ea1dd31f07
Brunno Barbosa Mendes <brunnomendesinfo@gmail.com>
2017-09-15 18:37:29 -0300

    - Adicionar url para pedir todos os objetos cadastrados

--- site/api_rest/urls.py
+++ site/api_rest/urls.py
@@ -6,4 +6,5 @@ app_name = 'api_rest'
 urlpatterns = [
     url(r'^movimentacoes_aberto/(?P<user_id>\d+)', views.MovimentacaoAbertoServiceView.as_view(), name='movimentacoes_aberto'),
     url(r'^devolver_objeto/', views.DevolverObjetoServiceView.as_view(), name='devolver_objeto'),
+    url(r'^exibir_objetos/', views.ObjetoServiceView.as_view(), name='exibir_objetos'),
 ]
