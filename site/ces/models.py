2d2285837dcf37c4da4bbe8323fee97865ac32ef
Aluno <aluno@ifba.edu>
2017-09-25 21:33:46 -0300

    Alterado atributo de tipo objeto

--- site/ces/models.py
+++ site/ces/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class TipoObjeto(models.Model):
     id = models.AutoField(primary_key=True, blank=False, null=False)
-    nome = models.CharField(max_length=50, blank=True, null=True)
+    nome = models.CharField(max_length=50, blank=False, null=True)
 
     def __str__(self):
         return self.nome
@@ -18,7 +18,7 @@ class TipoObjeto(models.Model):
 class Objeto(models.Model):
     id = models.AutoField(primary_key=True, blank=False, null=False)
     codigo = models.CharField(unique=True, max_length=50, blank=False, null=True)
-    nome = models.CharField(max_length=50, blank=False, null=True)
+    nome = models.CharField(unique=True, max_length=50, blank=False, null=True)
     tipoObjeto_id = models.ForeignKey(TipoObjeto)
 
     def __str__(self):
