b90d57d5328fc28dc870e9ff368840eaee83feb6
EliakinCosta <eliakim170@gmail.com>
2017-09-28 16:11:08 -0300

    Incluir staticfiles quando estiver em modo debug

--- site/cessite/urls.py
+++ site/cessite/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api_rest/', include('api_rest.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
