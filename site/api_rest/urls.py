d946ac04eba05adf11a77bd2c3a7eca2d56626fc
Brunno Barbosa Mendes <brunnomendesinfo@gmail.com>
2017-10-01 12:43:44 -0300

    - Adicionados serviços rest: 	Listar um objeto 	Listar todos os objetos disponíveis (não emprestados) 	Listar todos os objetos disponíveis para um usuário 	Listar todos os objetos emprestados para um usuário (histórico) 	Listar todos os objetos empresatos e não devolvidos para um usuário
    	Transferir um objeto

--- site/api_rest/urls.py
+++ site/api_rest/urls.py
@@ -4,8 +4,13 @@ from api_rest import views
 
 app_name = 'api_rest'
 urlpatterns = [
-    url(r'^movimentaca_aberta/(?P<user_id>\d+)', views.MovimentacaoAbertaServiceView.as_view(), name='movimentacoes_aberto'),
+    url(r'^exibir_objetos/', views.ObjetosServiceView.as_view(), name='exibir_objetos'),
+    url(r'^exibir_objeto/(?P<objeto_id>\d+)', views.ObjetoServiceView.as_view(), name='exibir_objeto'),
     url(r'^emprestar_objeto/', views.EmprestarObjetoServiceView.as_view(), name='emprestar_objeto'),
     url(r'^devolver_objeto/', views.DevolverObjetoServiceView.as_view(), name='devolver_objeto'),
-    url(r'^exibir_objetos/', views.ObjetoServiceView.as_view(), name='exibir_objetos'),
+    url(r'^transferir_objeto/', views.TransferirObjetoServiceView.as_view(), name='transferir_objeto'),
+    url(r'^objetos_disponiveis/', views.ObjetoDisponivelServiceView.as_view(), name='objetos_disponiveis'),
+    url(r'^movimentacoes_usuario/(?P<user_id>\d+)', views.MovimentacaoUsuarioServiceView.as_view(), name='movimentacoes_usuario'),
+    url(r'^objetos_disponiveis_usuario/(?P<user_id>\d+)', views.ObjetoDisponivelUsuarioServiceView.as_view(), name='objetos_disponiveis_usuario'),
+    url(r'^movimentacoes_abertas_usuario/(?P<user_id>\d+)', views.MovimentacaoAbertaUsuarioServiceView.as_view(), name='movimentacoes_abertas_usuario'),
 ]
