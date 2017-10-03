76c7e7cf848e15da33d17d106030ac6f362ab161
alunosifba <adsweb@gmatch.org>
2017-10-03 18:46:31 -0300

    Cadastro de usuário e grupos

--- site/ces/admin.py
+++ site/ces/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
-from .models import TipoObjeto
-from .models import Objeto
-from .models import PerfilUsuario
-from .models import Usuario
-from .models import Movimentacao
-from .models import Permissao_Objeto_x_Usuario
-from .models import Permissao_Objeto_x_PerfilUsuario
-from .models import AdminWeb
+from .models import *
 
 #para tornar models visível no site
 admin.site.register(TipoObjeto)
 admin.site.register(Objeto)
-admin.site.register(PerfilUsuario)
-admin.site.register(Usuario)
 admin.site.register(Movimentacao)
-admin.site.register(Permissao_Objeto_x_Usuario)
-admin.site.register(Permissao_Objeto_x_PerfilUsuario)
-admin.site.register(AdminWeb)
+admin.site.register(Aluno)
+admin.site.register(Professor)
+admin.site.register(Funcionario)
+admin.site.register(Departamento)
+admin.site.register(Setor)
+admin.site.register(Grupo)
+admin.site.register(Permissao)
+
+
+
+
+
+
+
+
+
--- site/ces/models.py
+++ site/ces/models.py
@@ -24,25 +24,11 @@ class Objeto(models.Model):
     def __str__(self):
         return self.nome
 
-
-class PerfilUsuario(models.Model):
-    id = models.AutoField(primary_key=True, blank=False, null=False)
-    nome = models.CharField(max_length=50, blank=True, null=False)
-
-    def __str__(self):
-        return self.nome
-
-    class Meta:
-        verbose_name = "Perfil de usuário"
-        verbose_name_plural = "Perfis de usuário"
-
-
 class Usuario(models.Model):
     id= models.AutoField(primary_key=True, blank=False, null=False)
     nome = models.CharField(max_length=250, blank=True, null=True)
-    email = models.CharField(max_length=50, unique=True, blank=False,)
+    email = models.CharField(max_length=50, unique=True, blank=False)
     senha = models.TextField(blank=False, null=False)
-    perfilUsuario_id = models.ForeignKey(PerfilUsuario)
 
     def __str__(self):
         return self.nome
@@ -50,49 +36,82 @@ class Usuario(models.Model):
     class Meta:
         verbose_name = "Usuário"
         verbose_name_plural = "Usuários"
+        
+        
+class Aluno(Usuario):
+    
+    class Meta:
+        verbose_name = "Aluno"
+        verbose_name_plural = "Alunos"
+
+class Departamento(models.Model):
+    id= models.AutoField(primary_key=True, blank=False, null=False)
+    descricao= models.CharField(max_length=50, unique=True, blank=False)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        verbose_name = "Departamento"
+        verbose_name_plural = "Departamentos"
+
+class Professor(Usuario):
+    departamento = models.ForeignKey(Departamento)
+
+    class Meta:
+        verbose_name = "Professor"
+        verbose_name_plural = "Professores"
+
+
+class Setor(models.Model):
+    id = models.AutoField(primary_key=True, blank=False, null=False)
+    descricao= models.CharField(max_length=50, unique=True, blank=False)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        verbose_name = "Setor"
+        verbose_name_plural = "Setores"
+
+class Funcionario(Usuario):
+    setor= models.ForeignKey(Setor)
+    class Meta:
+        verbose_name = "Funcionario"
+        verbose_name_plural = "Funcionarios"
+
+class Grupo(models.Model):
+    id = models.AutoField(primary_key=True, blank=False, null=False)
+    usuarios = models.ManyToMany(Usuario)
+    permissoes = models.ManyToMany(Permissao)
+    descricao = models.CharField(max_length=50, unique=True, blank=False,)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    data_alteracao = models.DateTimeField(default=timezone.now)
+    criado_por = models.ForeignKey('auth.User')
+    alterado_por = models.ForeignKey('auth.User')
+
+class Permissao(models.Model):
+    id  = models.AutoField(primary_key=True, blank=False, null=False)
+    descricao= models.CharField(max_length=50, unique=True, blank=False,)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        verbose_name = "Permissão"
+        verbose_name_plural = "Permissões"
 
 
 class Movimentacao(models.Model):
-    id= models.AutoField(primary_key=True, blank=False, null=False)
-    retirada = models.DateTimeField(null=True, blank=True)
-    devolucao = models.DateTimeField(null=True, blank=True)
+    id = models.AutoField(primary_key=True, blank=False, null=False)
+    retirada = models.DateTimeField(default=timezone.now)
+    devolucao = models.DateTimeField(default=timezone.now)
     objeto_id = models.ForeignKey(Objeto)
     usuario_id = models.ForeignKey(Usuario)
 
     def __str__(self):
-        return str("{0} para {1}").format(self.objeto_id, self.usuario_id)
+        return self.id
 
     class Meta:
         verbose_name = "Movimentação"
         verbose_name_plural = "Movimentações"
-
-
-class Permissao_Objeto_x_Usuario(models.Model):
-    id= models.AutoField(primary_key=True, blank=False, null=False)
-    objeto_id = models.ForeignKey(Objeto)
-    usuario_id = models.ForeignKey(Usuario)
-
-    def __str__(self):
-        return str(self.id)
-
-
-class Permissao_Objeto_x_PerfilUsuario(models.Model):
-    id= models.AutoField(primary_key=True, blank=False, null=False)
-    tipoObjeto_id = models.ForeignKey(TipoObjeto)
-    perfilUsuario_id = models.ForeignKey(PerfilUsuario)
-
-    def __str__(self):
-        return str(self.id)
-
-
-class AdminWeb(models.Model):
-    id= models.AutoField(primary_key=True, blank=False, null=False)
-    tipoObjeto_id = models.ForeignKey(TipoObjeto)
-    usuario_id = models.ForeignKey(Usuario)
-
-    def __str__(self):
-        return str(self.id)
-
-    class Meta:
-        verbose_name = "Admin web"
-        verbose_name_plural = "Admins web"
