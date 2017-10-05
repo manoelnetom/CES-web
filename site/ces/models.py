7d94916182921a639d1eb4fd62edb3b98daafb92
Helislana Damasceno de Freitas <helislana.freitas@solutis.com.br>
2017-10-04 21:02:05 -0300

    corrigindo bug no model departamento

--- site/ces/models.py
+++ site/ces/models.py
@@ -68,7 +68,7 @@ class Setor(models.Model):
     descricao= models.CharField(max_length=50, unique=True, blank=False)
 
     def __str__(self):
-        return self.id
+        return self.descricao
 
     class Meta:
         verbose_name = "Setor"
