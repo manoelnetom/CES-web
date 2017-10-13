d9cdf679c4804fc10701f67edecb4e89db3baf9b
BrunnoMendes <brunnomendesinfo@gmail.com>
2017-10-13 16:51:36 -0300

    - Remover implementacao incorreta no serializers (Profile)

--- site/api_rest/serializers.py
+++ site/api_rest/serializers.py
@@ -12,23 +12,14 @@ class ObjetoSerializer(serializers.ModelSerializer):
 
 class MovimentacaoSerializer(serializers.ModelSerializer):
 
-    objeto_id = ObjetoSerializer()
     class Meta:
         model = models.Movimentacao
         fields = ('id', 'retirada', 'devolucao', 'objeto_id', 'usuario_id', 'status')
         depth = 1
 
 
-class PerfilUsuarioSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.PerfilUsuario
-        fields = ('id', 'nome')
-        depth = 1
-
 class UsuarioSerializer(serializers.ModelSerializer):
 
-    profileName = PerfilUsuarioSerializer()
     class Meta:
         model = models.Usuario
         fields = ('id', 'name', 'email', 'profileName')
