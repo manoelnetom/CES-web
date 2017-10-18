e0141c3985f3e0dd8365d28c59c2c9f9b40b2931
Brunno Barbosa Mendes <brunnomendesinfo@gmail.com>
2017-10-18 19:06:28 -0300

    - Alterar a resposta do serializer de movimentacao

--- site/api_rest/serializers.py
+++ site/api_rest/serializers.py
@@ -10,15 +10,6 @@ class ObjetoSerializer(serializers.ModelSerializer):
         depth = 1
 
 
-class MovimentacaoSerializer(serializers.ModelSerializer):
-    reserva = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
-
-    class Meta:
-        model = models.Movimentacao
-        fields = ('id', 'retirada', 'devolucao', 'reserva', 'objeto_id', 'usuario_id', 'status')
-        depth = 1
-
-
 class UsuarioSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -27,7 +18,19 @@ class UsuarioSerializer(serializers.ModelSerializer):
         depth = 1
 
 
+class MovimentacaoSerializer(serializers.ModelSerializer):
+    usuario_id = UsuarioSerializer()
+    reserva = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+    class Meta:
+        model = models.Movimentacao
+        fields = ('id', 'retirada', 'devolucao', 'reserva', 'objeto_id', 'usuario_id', 'status')
+        depth = 1
+
+
 class TransferenciaSerializer(serializers.ModelSerializer):
+    movimentacao_id_origem = MovimentacaoSerializer()
+    movimentacao_id_destino = MovimentacaoSerializer()
 
     class Meta:
         model = models.Transferencia
